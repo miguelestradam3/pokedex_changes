b4ae0989cc1734cb7ac799201f995850c84c1f33
unknown <miguelestradam36@gmail.com>
2026-07-02 18:43:37 -0600

    Project from another person plus some development

--- js/script.js
+++ js/script.js
@@ -1,13 +1,36 @@
 const pokemonName = document.querySelector('.pokemon__name');
 const pokemonNumber = document.querySelector('.pokemon__number');
 const pokemonImage = document.querySelector('.pokemon__image');
+const pokemonTypes = document.querySelector('.pokemon__types');
 
 const form = document.querySelector('.form');
+const types = document.querySelector('.types');
 const input = document.querySelector('.input__search');
 const buttonPrev = document.querySelector('.btn-prev');
 const buttonNext = document.querySelector('.btn-next');
 
-let searchPokemon = 1;
+const typeColors = {
+    normal: "#A8A77A",
+    fire: "#EE8130",
+    water: "#6390F0",
+    electric: "#F7D02C",
+    grass: "#7AC74C",
+    ice: "#96D9D6",
+    fighting: "#C22E28",
+    poison: "#A33EA1",
+    ground: "#E2BF65",
+    flying: "#A98FF3",
+    psychic: "#F95587",
+    bug: "#A6B91A",
+    rock: "#B6A136",
+    ghost: "#735797",
+    dragon: "#6F35FC",
+    dark: "#705746",
+    steel: "#B7B7CE",
+    fairy: "#D685AD"
+};
+
+let searchPokemon = 6;
 
 const fetchPokemon = async (pokemon) => {
   const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
@@ -22,6 +45,7 @@ const renderPokemon = async (pokemon) => {
 
   pokemonName.innerHTML = 'Loading...';
   pokemonNumber.innerHTML = '';
+  types.innerHTML = '';
 
   const data = await fetchPokemon(pokemon);
 
@@ -31,7 +55,23 @@ const renderPokemon = async (pokemon) => {
     pokemonNumber.innerHTML = data.id;
     pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
     input.value = '';
-    searchPokemon = data.id;
+    data.types.forEach(type => {
+
+        const name = type.type.name;
+
+        types.innerHTML += `
+            <span
+                class="badge m-1"
+                style="
+                    background:${typeColors[name]};
+                    color:white;
+                ">
+                ${name}
+            </span>
+        `;
+
+    });
+
   } else {
     pokemonImage.style.display = 'none';
     pokemonName.innerHTML = 'Not found :c';
